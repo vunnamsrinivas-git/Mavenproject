efe406856a23571a1559b0fd0f5b8b328077578e
root <root@ip-172-31-46-3.ap-south-1.compute.internal>
2019-11-26 10:28:26 +0000

    commited to mavenprojects

---
+++ src/main/java/sriramtechnologies/App.java
@@ -0,0 +1,13 @@
+package sriramtechnologies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
